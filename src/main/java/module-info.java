0d92260032e146173753dd541578f772cff09dca
Nicolás Francisco López-ibarra Lúque <84680379+NMachinne@users.noreply.github.com>
2023-01-16 11:22:07 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.Aulas.proyectoAulasPSP {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.Aulas.proyectoAulasPSP to javafx.fxml;
-    exports com.Aulas.proyectoAulasPSP;
-}
